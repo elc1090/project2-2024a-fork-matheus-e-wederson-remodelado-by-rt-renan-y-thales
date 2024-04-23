11ecf3015432596acf62f91ebafb8d88730bc186
wedersonf <wedymf@hotmail.com>
2024-04-23 08:57:07 -0300

    style: show when billing type is selected

--- src/components/billing-type-button.tsx
+++ src/components/billing-type-button.tsx
@@ -16,12 +16,13 @@ export function BillingTypeButton({
   isSelected = false,
   ...props
 }: BillingTypeButtonProps) {
+  console.log(isSelected)
   return (
     <button
       type="button"
       className={clsx(
-        isSelected && 'border-zinc-300',
-        'flex h-24 w-full flex-col items-center justify-center gap-2 rounded border-2 border-zinc-700 outline-none hover:bg-zinc-700/50 focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900',
+        'flex h-24 w-full flex-col items-center justify-center gap-2 rounded border-2 outline-none hover:bg-zinc-700/50 focus-visible:ring-2 focus-visible:ring-zinc-400 focus-visible:ring-offset-2 focus-visible:ring-offset-zinc-900',
+        isSelected ? 'border-zinc-300' : 'border-zinc-700 ',
       )}
       {...props}
     >
