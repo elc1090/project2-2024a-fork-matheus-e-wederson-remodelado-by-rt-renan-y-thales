8d0237a27c4b0b26600e8d99ef8a610a673fe070
wedersonf <wedymf@hotmail.com>
2024-04-23 07:51:57 -0300

    build: fix schema to postgresql

---
+++ prisma/migrations/20240423104716_/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "products" ALTER COLUMN "price" SET DATA TYPE DOUBLE PRECISION;
+
+-- AlterTable
+ALTER TABLE "transaction" ALTER COLUMN "amount" SET DATA TYPE DOUBLE PRECISION;
